6eaaa3fe900596138088fc611dda54fe4b0eb6ca
Dmitry.Shelepev <dmitry.shelepev@twobits.games>
2025-12-06 08:00:35 +0300

    Clearing

--- clean.bat
+++ clean.bat
@@ -17,8 +17,8 @@ rem Unreal Engine
 del /Q Binaries
 rd /S /Q Binaries
 rem Build - build settings
-del /Q Build
-rd /S /Q Build
+: del /Q Build
+: rd /S /Q Build
 rem Intermediate - temporary files generated when you compile your code
 del /Q Intermediate
 rd /S /Q Intermediate
@@ -26,3 +26,11 @@ del /Q DerivedDataCache
 rd /S /Q DerivedDataCache
 del /Q Script
 rd /S /Q Script
+
+rem  Saved data - crashes, logs, etc.
+if exist Saved (
+    del /Q Saved\Crashes 2>nul
+    rd /S /Q Saved\Crashes 2>nul
+    del /Q Saved\Logs 2>nul
+    rd /S /Q Saved\Logs 2>nul
+)
